f593a2247adc37b9c6057a9f01b541714fce35ce
MasanoriIwakura <hagobutler@yahoo.co.jp>
2023-04-23 22:27:03 +0900

    Add gorm connection

--- app/go.mod
+++ app/go.mod
@@ -4,6 +4,8 @@ go 1.20
 
 require (
 	github.com/99designs/gqlgen v0.17.30
+	github.com/go-sql-driver/mysql v1.7.0
+	github.com/jinzhu/gorm v1.9.16
 	github.com/vektah/gqlparser/v2 v2.5.1
 )
 
@@ -12,6 +14,7 @@ require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/hashicorp/golang-lru/v2 v2.0.1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/urfave/cli/v2 v2.24.4 // indirect
--- app/server.go
+++ app/server.go
@@ -7,18 +7,24 @@ import (
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/MasanoriIwakura/golang-todo-webapp/config"
 	"github.com/MasanoriIwakura/golang-todo-webapp/graph"
 )
 
 const defaultPort = "8080"
 
 func main() {
+	db, err := config.NewDB()
+	if err != nil {
+		panic(err.Error())
+	}
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{DB: db}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
